e70d07aeaa3e607882081e07fbfe860c63d1bdb8
rajshreek651 <rajshreek651@gmail.com>
2026-06-03 23:06:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Todo List using React
+
+1. Create the directory to do the whole project.
+
+2. In the Terminal for CWD, run:
+    > npm create vite@latest my-todo-app --> to install required dependencies
+    > y --> to proceed
+    > Select other options for asked questions using ARROW KEYS (UP & DOWN)
+    > open the link provided i.e. 'http://localhost:5173/'
+
+3. Clear the unrequired icons, assets, etc. . Also clear the unnnecessary code from App.jsx, etc.
+
+4. In ./src, create a new file with name 'TodoList.jsx'
+
+5. In ./src/TodoList.jsx, check by wrting the code if it works properly or not:
+    export default function TodoList() {
+        return (
+            <div>
+                <h4>Todo List</h4>
+            </div>
+        )
+    }
+
+6. Now after writing the basic code for Todo, we want whatever task is written in 'Add Task bar' and added button is clicked the todo should get added in the <ul></ul> as a list element.
+
+We want the todos to get saved in an array named 'todos[]'. Whenever we add new task a new item is added to the array and we want it to get added to the unordered list as well, so our 'todos[]' will be one of the 'STATE VARIABLES', so that any changes made in todos[] will get reflected in the UI (User Interface), i.e. our component should be re-rendered.
+
+* LOGIC FOR <ul></ul>:
+We know if we want to render an array we use 'map' method.
+
+Now to add new tasks, we need to add functionality to the 'Add Task' button.
+
+For this we first need to track what 'new todo' is being written in the input box. So for this as well, we create a variable called 'newTodo' which is again a 'STATE VARIABLE'.
+
+So WE 2 STATE VARIABLES i.e. variables which can change the UI:
+(i) todos[] array which will be triggered when 'Add Task' button is clicked
+(ii) newTodo varaible which will be triggered whenever there is a change in input box
+
+When we perform changes in the value of input box, we can Inspect the changes:
+Right click on web page --> Select Inspect --> Go to '>>' arrows beside console tab --> Select Components
+
+We can see changes triggered in state variable 'newTodo' when value of input box changes, 
+Similarly, changes triggered in state variable 'todo' when 'Add Task' button is clicked
+
+* In React when:
+(i) When updating a state variablewhich is an ARRAY and want to render, we use: 'spread opr' i.e. ...{}
+(ii) When updating a state variable which is an ARRAY and want to render, we use: 'map' method
+(iii) When deleting a state variable which is an ARRAY, we use: 'filter' method
+
+
+# FEATURE OPTIMIZED
+Even when no value was added to the input bar and 'Add Task' button was clicked, an empty task was getting created which can result in unnecessary usage of memory.
+So we can give condition as:
+    if(newTodo.trim() === "") { // If the input field is empty or contains only whitespace, we don't add a new task
+        return;
+    }
---
+++ src/TodoList.jsx
@@ -0,0 +1,97 @@
+import {useState} from 'react';
+import {v4 as uuidv4} from 'uuid'; 
+
+export default function TodoList() {
+
+    // State variable 'todos' to hold the list of tasks
+    let [todos, setTodos] = useState([{task: "sample task", id: uuidv4(), isDone: false}]); // todo is an object with 'task' as the task description, &, todos is the list of all the objects
+    let [newTodo, setNewTodo] = useState("");
+
+    // Creating a Handler Function to add a new task to the list
+    let addNewTask = () => {
+        if(newTodo.trim() === "") { // If the input field is empty or contains only whitespace, we don't add a new task
+            return;
+        }
+        setTodos((prevTodos) => {
+            return [...prevTodos, {task: newTodo, id: uuidv4(), isDone: false}]
+        }); // Using the spread operator to create a new array with the existing tasks and the new task
+        setNewTodo(""); // Clear the input field after adding the task
+    };
+
+    let updateTodoValue = (event) => {
+        setNewTodo(event.target.value);
+    };
+
+    let deleteTodo = (id) => {
+        // console.log("Task to be deleted: ", id);
+        setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id)); // will make the list of all the todos whose 'id' doesn't match with passed 'id', then this todo list will be rendered and the task with the passed 'id' will be removed from the UI
+    };
+
+    let markAsDone = (id) => {
+        setTodos((prevTodos) => prevTodos.map((todo) => {
+            if(todo.id === id) {
+                return {...todo, isDone: true}; // If the id matches, we create a new object with the same properties as the existing todo but with 'isDone' set to true
+            }else{
+                return todo; // If the id doesn't match, we return the existing todo object without any changes
+            }
+        }));
+    }
+
+    let markAsUndone = (id) => {
+        setTodos((prevTodos) => prevTodos.map((todo) => {
+            if(todo.id === id) {
+                return {...todo, isDone: false}; // If the id matches, we create a new object with the same properties as the existing todo but with 'isDone' set to false
+            }else{
+                return todo; // If the id doesn't match, we return the existing todo object without any changes
+            }
+        }));
+    };
+
+    let markAllAsDone = () => {
+        setTodos((prevTodos) => prevTodos.map((todo) => {
+            return {...todo, isDone: true}; // We create a new array of todos where all the tasks are marked as done
+        }));
+    };
+
+    let markAllAsUndone = () => {
+        setTodos((prevTodos) => prevTodos.map((todo) => {
+            return {...todo, isDone: false}; // We create a new array of todos where all the tasks are marked as undone
+        }));
+    };
+
+    return (
+        <div>
+            <div className='add-task'>
+                <input placeholder="Add a new task" value={newTodo} onChange={updateTodoValue}/>
+                &nbsp;&nbsp;
+                <button onClick={addNewTask}>Add Task</button>  
+            </div>
+
+            <div className='mark-all'>
+                <button onClick={markAllAsDone}>Mark All as Done</button>
+                <button onClick={markAllAsUndone}>Mark All as Undone</button>
+            </div>
+
+            <h2>Tasks Todo</h2>
+            <ul>
+                {
+                    todos.map((todo, index) => (
+                        <li key={todo.id}>
+                            <span style={todo.isDone ? {textDecorationLine: "line-through"} : {}}> {todo.task} </span>
+                            <span className="todo-options">
+                                {todo.isDone ? (
+                                    <button className="todo-opt" onClick={() => markAsUndone(todo.id)} style={{textDecorationLine: "line-through"}}><i class="fa-solid fa-square-check" ></i></button>
+                                ) : (
+                                    <button className="todo-opt" onClick={() => markAsDone(todo.id)} style={{textDecorationLine: "none"}}><i class="fa-solid fa-square-check" style={{color: "transparent", backgroundColor: "transparent", border: "3px solid black", borderRadius: "10px", fontSize: "1.25rem"}}></i></button>
+                                )}
+                                <button className="todo-opt" onClick={() => deleteTodo(todo.id)}><i class="fa-solid fa-circle-xmark"></i></button> 
+                            </span>
+                        </li>
+                    ))
+                    // If we directly do as onClick={deleteTodo(index)}, then it will directly execute delete and nothing will happen on clicking the delete button
+                    // So we make it as an 'arrow function' and pass
+                }
+            </ul>
+        </div>
+    )
+}
